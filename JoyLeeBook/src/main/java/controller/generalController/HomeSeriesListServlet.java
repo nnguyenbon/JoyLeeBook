257651e567660febb39e7a8d8bcfaa69742459fc
Bon Nguyen <133227369+nnguyenbon@users.noreply.github.com>
2025-07-07 10:05:30 +0700

    Update HomeSeriesListServlet.java

--- JoyLeeBook/src/main/java/controller/generalController/HomeSeriesListServlet.java
+++ JoyLeeBook/src/main/java/controller/generalController/HomeSeriesListServlet.java
@@ -27,32 +27,6 @@ import model.Series;
 @WebServlet(name = "HomeSeriesListServlet", urlPatterns = { "/home" })
 public class HomeSeriesListServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     * 
-     * @param request  servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HomeSeriesListServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HomeSeriesListServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
     // + sign on the left to edit the code.">
     /**
@@ -80,22 +54,9 @@ public class HomeSeriesListServlet extends HttpServlet {
 
         } catch (Exception e) {
             e.printStackTrace();
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "database error");
+            request.setAttribute("error", "Cannot get the Series List.");
+            request.getRequestDispatcher("/views/error.jsp").forward(request, response);
         }
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     * 
-     * @param request  servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /**
