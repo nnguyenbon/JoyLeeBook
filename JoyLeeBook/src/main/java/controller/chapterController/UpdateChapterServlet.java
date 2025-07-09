c8f1d125e0263aac0876f72bcd0f9588c12e56b1
TrungNguyen <trunguyen.104@gmail.com>
2025-07-09 12:32:49 +0700

    add new servlet UpdateChapter and RemoveSavedSeries

--- JoyLeeBook/src/main/java/controller/chapterController/UpdateChapterServlet.java
+++ JoyLeeBook/src/main/java/controller/chapterController/UpdateChapterServlet.java
@@ -2,51 +2,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.chapterController;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import dao.ChapterDAO;
+import dao.SeriesDAO;
+import db.DBConnection;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.Chapter;
+import model.Series;
 
 /**
  *
  * @author PC
  */
-@WebServlet(name="UpdateChapterServlet", urlPatterns={"/updateChapter"})
+@WebServlet(name = "UpdateChapterServlet", urlPatterns = {"/updateChapter"})
 public class UpdateChapterServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UpdateChapterServlet</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UpdateChapterServlet at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+    // + sign on the left to edit the code.">
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,12 +35,34 @@ public class UpdateChapterServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+            throws ServletException, IOException {
+        try {
+            int chapterId = Integer.parseInt(request.getParameter("chapterId"));
+            ChapterDAO chapterDAO = new ChapterDAO(DBConnection.getConnection());
+            Chapter chapter = chapterDAO.getChapterById(chapterId);
 
-    /** 
+            if (chapter == null) {
+                response.sendRedirect("views/error.jsp");
+                return;
+            }
+
+            SeriesDAO seriesDAO = new SeriesDAO(DBConnection.getConnection());
+            Series series = seriesDAO.getSeriesById(chapter.getSeriesId());
+            request.setAttribute("chapter", chapter);
+            request.setAttribute("seriesTitle", series.getSeriesTitle());
+
+            request.getRequestDispatcher("views/chapter/editChapter.jsp").forward(request, response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            request.setAttribute("error", "Cannot get Chapter detail.");
+            request.getRequestDispatcher("views/error.jsp").forward(request, response);
+        }
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -67,12 +70,49 @@ public class UpdateChapterServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+            throws ServletException, IOException {
+
+        try {
+            int chapterId = Integer.parseInt(request.getParameter("chapterId"));
+            String seriesTitle = request.getParameter("seriesTitle");
+            int chapterIndex = Integer.parseInt(request.getParameter("chapterIndex"));
+            String chapterTitle = request.getParameter("chapterTitle");
+            String content = request.getParameter("content");
+            Chapter chapter = new Chapter(chapterId, chapterIndex, chapterTitle, content);
+
+            if (chapterTitle == null || chapterTitle.trim().isEmpty()) {
+                request.setAttribute("message", "Chapter Title cannot empty");
+                request.setAttribute("chapter", chapter);
+                request.setAttribute("seriesTitle", seriesTitle);
+                request.getRequestDispatcher("views/chapter/editChapter.jsp").forward(request, response);
+            }
+            ChapterDAO chapterDAO = new ChapterDAO(DBConnection.getConnection());
+            chapterDAO.updateChapter(chapter);
+            request.setAttribute("message", "updated successfully!");
+            request.getRequestDispatcher("views/chapter/readChapter.jsp").forward(request, response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            int chapterId = Integer.parseInt(request.getParameter("chapterId"));
+            String seriesTitle = request.getParameter("seriesTitle");
+            int chapterIndex = Integer.parseInt(request.getParameter("chapterIndex"));
+            String chapterTitle = request.getParameter("chapterTitle");
+            String content = request.getParameter("content");
+
+            Chapter chapter = new Chapter(chapterId, chapterIndex, chapterTitle, content);
+
+            request.setAttribute("chapter", chapter);
+            request.setAttribute("seriesTitle", seriesTitle);
+            request.setAttribute("message", "update failed");
+            request.getRequestDispatcher("views/chapter/editChapter.jsp").forward(request, response);
+        }
+
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
--- JoyLeeBook/src/main/java/controller/seriesController/RemoveSavedSeriesServlet.java
+++ JoyLeeBook/src/main/java/controller/seriesController/RemoveSavedSeriesServlet.java
@@ -2,51 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.seriesController;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import dao.SeriesDAO;
+import db.DBConnection;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  *
  * @author PC
  */
-@WebServlet(name="RemoveSavedSeriesServlet", urlPatterns={"/removeSavedSeries"})
+@WebServlet(name = "RemoveSavedSeriesServlet", urlPatterns = {"/removeSavedSeries"})
 public class RemoveSavedSeriesServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RemoveSavedSeriesServlet</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RemoveSavedSeriesServlet at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,25 +31,37 @@ public class RemoveSavedSeriesServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+            throws ServletException, IOException {
+//        HttpSession session = request.getSession(false);
+//
+//        User user = (User) session.getAttribute("user");
+//        if (!user.getRoleName().equals("admin")) {
+//            request.setAttribute("error", "Bạn không có quyền truy cập chức năng này.");
+//            request.getRequestDispatcher("views/error.jsp").forward(request, response);
+//            return;
+//        }
+        try {
+            int seriesId = Integer.parseInt(request.getParameter("seriesId"));
+            SeriesDAO seriesDAO = new SeriesDAO(DBConnection.getConnection());
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+            if (seriesDAO.deleteSeries(seriesId)) {
+                request.setAttribute("message", "Delete successfully!");
+                request.getRequestDispatcher("adminDashboard").forward(request, response);
+            } else {
+                request.setAttribute("message", "Delete failed!");
+                request.getRequestDispatcher("adminDashboard").forward(request, response);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            request.setAttribute("error", "Delete failed!");
+            request.getRequestDispatcher("views/error.jsp").forward(request, response);
+        }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
