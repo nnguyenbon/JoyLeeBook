62184c081ea14fe96b45926ed517d1a402954809
Toan <tktoan10a1@gmail.com>
2025-07-19 00:57:01 +0700

    change webfilter urlpatterns, add errorServlet

--- JoyLeeBook/src/main/java/controller/generalController/ErrorServlet.java
+++ JoyLeeBook/src/main/java/controller/generalController/ErrorServlet.java
@@ -2,10 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.generalController;
 
-import dao.SeriesDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -13,36 +11,31 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import db.DBConnection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import model.Series;
 
 /**
  *
- * @author PC
+ * @author KHAI TOAN
  */
-@WebServlet(name="AdminDashboardServlet", urlPatterns={"/adminDashboard"})
-public class AdminDashboardServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+@WebServlet(name = "ErrorServlet", urlPatterns = {"/error"})
+public class ErrorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-       
-    } 
+            throws ServletException, IOException {
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -50,22 +43,18 @@ public class AdminDashboardServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        try {
-            SeriesDAO seriesDao = new SeriesDAO(DBConnection.getConnection());
-            List<Series> allSeries = seriesDao.getAllSeries();
-            request.setAttribute("allSeries", allSeries);
-        } catch (SQLException e){
-            request.setAttribute("errorMessage", "Lỗi kết nối DB " + e);
-            request.getRequestDispatcher("/WEB-INF/views/adminDashboard.jsp").forward(request, response);
-        } catch (Exception e) { 
-            e.printStackTrace();
+            throws ServletException, IOException {
+        Object error = request.getAttribute("error");
+        if (error == null) {
+            response.sendRedirect(request.getContextPath() + "/");
+            return;
         }
-        request.getRequestDispatcher("/WEB-INF/views/adminDashboard.jsp").forward(request, response);
-    } 
+        request.getRequestDispatcher("/WEB-INF/views/error.jsp").forward(request, response);
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -73,12 +62,13 @@ public class AdminDashboardServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
