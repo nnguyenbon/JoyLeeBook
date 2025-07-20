8c1a9736aecc35823b32f3d13c2ac151ef53ca92
Bon Nguyen <133227369+nnguyenbon@users.noreply.github.com>
2025-07-20 19:29:28 +0700

    Update ForwardChapterServlet.java

--- JoyLeeBook/src/main/java/controller/chapterController/ForwardChapterServlet.java
+++ JoyLeeBook/src/main/java/controller/chapterController/ForwardChapterServlet.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import model.Chapter;
+import static utils.Validator.*;
 
 /**
  *
