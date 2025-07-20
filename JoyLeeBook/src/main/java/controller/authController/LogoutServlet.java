6282bbb01148421a928eae6db480801837604a76
Bon Nguyen <133227369+nnguyenbon@users.noreply.github.com>
2025-07-20 16:07:22 +0700

    Update LogoutServlet.java

--- JoyLeeBook/src/main/java/controller/authController/LogoutServlet.java
+++ JoyLeeBook/src/main/java/controller/authController/LogoutServlet.java
@@ -46,18 +46,4 @@ public class LogoutServlet extends HttpServlet {
         // Redirect to login page after logout 
         response.sendRedirect("login");
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
 }
