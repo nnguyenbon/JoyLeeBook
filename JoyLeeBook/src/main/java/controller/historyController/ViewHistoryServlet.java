7b44199e45076a16a0ad2555e4a8e1f0f917dcbe
Bon Nguyen <133227369+nnguyenbon@users.noreply.github.com>
2025-07-20 19:53:53 +0700

    Update ViewHistoryServlet.java

--- JoyLeeBook/src/main/java/controller/historyController/ViewHistoryServlet.java
+++ JoyLeeBook/src/main/java/controller/historyController/ViewHistoryServlet.java
@@ -5,11 +5,14 @@ import dao.UserDAO;
 import db.DBConnection;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
 import model.HistoryReading;
 import model.User;
-
-import java.io.IOException;
+import static utils.Validator.*;
 import java.util.ArrayList;
 
 @WebServlet("/history")
