237b7cdab21fc793fcc7a3ad40a8c04d66570235
Minh Phuc <minhphuc3589@gmail.com>
2025-07-20 10:35:08 +0700

    Updated header_loggedIn and header_unLoggedIn

--- js/index.js
+++ js/index.js
@@ -1,46 +1,47 @@
-// const menu = document.getElementById('navbarNav')
-// const userProfile = document.getElementById('user-profile')
+const menu = document.getElementById('navbarNav')
+const userProfile = document.getElementById('user-profile')
+
+window.addEventListener('load', () => {
+
+    displayElement({
+        name: menu,
+        icon: 'menu-icon',
+        isShow: false
+    })
+
+    displayElement({
+        name: userProfile,
+        ico: 'user-profile-icon',
+        isShow: false
+    })
+
+    //    document.addEventListener("contextmenu", function (e) {
+    //        e.preventDefault();
+    //    });
+
+})
 
 
-// window.addEventListener('load', () => {
+const displayElement = (element) => {
+    /*
+     Notes:
+     element['name']: Main element's tag which is got by document.getElementById()
+     element['icon']: Icon's ID String which will handle 'click' event to show main element's tag
+     element['isShow']: Status of main element's tag
+     */
 
-//     displayElement({
-//         name: menu,
-//         icon: 'menu-icon',
-//         isShw: false
-//     })
+    if (element['name'] === null || element['icon'] === null) {
+        return
+    }
 
-//     displayElement({
-//         name: userProfile,
-//         ico: 'user-profile-icon',
-//         isShow: false
-//     })
+    document.getElementById(element['icon']).addEventListener('click', () => {
 
-// //    document.addEventListener("contextmenu", function (e) {
-// //        e.preventDefault();
-// //    });
-
-// })
-
-
-// const displayElement = (element) => {
-//     /*
-//      Notes:
-//      element['name']: Main element's tag which is got by document.getElementById()
-//      element['icon']: Icon's ID String which will handle 'click' event to show main element's tag
-//      element['isShow']: Status of main element's tag
-//      */
-
-//     if (element['name'] === null || element['icon'] === null) return
-
-//     document.getElementById(element['icon']).addEventListener('click', () => {
-
-//         if (element['isShow']) {
-//             element['name'].classList.remove('d-flex')
-//             element['isShow'] = false
-//         } else {
-//             element['name'].classList.add('d-flex')
-//             element['isShow'] = true
-//         }
-//     })
-// }
+        if (element['isShow']) {
+            element['name'].classList.remove('d-flex')
+            element['isShow'] = false
+        } else {
+            element['name'].classList.add('d-flex')
+            element['isShow'] = true
+        }
+    })
+}
