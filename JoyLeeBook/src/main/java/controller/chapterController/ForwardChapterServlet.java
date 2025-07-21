8e0bd58d225ce36c02b893c6e15498069dee07bb
Toan <tktoan10a1@gmail.com>
2025-07-21 17:08:48 +0700

    fix bug

--- JoyLeeBook/src/main/java/controller/chapterController/ForwardChapterServlet.java
+++ JoyLeeBook/src/main/java/controller/chapterController/ForwardChapterServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import dao.ChapterDAO;
+import dao.SeriesDAO;
 import db.DBConnection;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,6 +12,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.Chapter;
+import model.Series;
 import static utils.Validator.isValidInteger;
 
 /**
@@ -30,7 +32,9 @@ public class ForwardChapterServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        java.sql.Connection conn = null;
         try {
+            conn = DBConnection.getConnection();
             String chapterIdStr = request.getParameter("chapterId");
 
             if (!isValidInteger(chapterIdStr)) {
@@ -40,7 +44,8 @@ public class ForwardChapterServlet extends HttpServlet {
             }
             int chapterId = Integer.parseInt(chapterIdStr);
 
-            ChapterDAO chapterDAO = new ChapterDAO(DBConnection.getConnection());
+            ChapterDAO chapterDAO = new ChapterDAO(conn);
+            SeriesDAO seriesDAO = new SeriesDAO(conn);
             Chapter currentChapter = chapterDAO.getChapterById(chapterId);
 
             if (currentChapter == null) {
@@ -52,6 +57,8 @@ public class ForwardChapterServlet extends HttpServlet {
             ArrayList<Chapter> allChapterOfSeries = chapterDAO.getAllChaptersBySeriesId(currentChapter.getSeriesId());
             Chapter newChapter = chapterDAO.getNextChapter(currentChapter.getSeriesId(),
                     currentChapter.getChapterIndex());
+            Series series = seriesDAO.getSeriesById(newChapter.getSeriesId());
+            newChapter.setSeriesTitle(series.getSeriesTitle());
             request.setAttribute("firstIndex", chapterDAO.getFirstChapterIndex(currentChapter.getSeriesId()));
             request.setAttribute("lastIndex", chapterDAO.getLastChapterIndex(currentChapter.getSeriesId()));
             request.setAttribute("chapter", newChapter);
